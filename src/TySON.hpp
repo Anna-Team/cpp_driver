f1dc45fec48179e1e885dede20d2e376655fc37b
FelixTheC <felixeisenmenger@gmx.net>
2023-02-02 23:11:45 +0100

    [FEAT] begin TySon value parsing

--- src/TySON.hpp
+++ src/TySON.hpp
@@ -7,10 +7,192 @@
 
 namespace tyson
 {
-
-    class TySONObject
+    enum class TySonType
     {
+        Number = 'n',
+        String = 's',
+        Bool = 'b',
+        Null = 'x',
+        Timestamp = 'u',
+        Link = 'l',
+        Vector = 'v',
+        Map = 'm',
+    };
 
+    class TySonObject
+    {
+        TySonType type_;
+        std::string value_;
+        std::vector<TySonObject> vector_ {};
+        std::map<std::string, TySonObject> map_ {};
+
+        void parse_vector_elements(std::string_view object)
+        {
+            auto end_type_sep = object.find_first_of('[') + 1;
+            auto end_value_sep = (object.size() - 1) - end_type_sep;
+
+            auto vector_data = object.substr(end_type_sep, end_value_sep);
+
+            while (true)
+            {
+                auto next_comma = vector_data.find_first_of(',');
+
+                if (next_comma == std::string::npos)
+                {
+                    break;
+                }
+                auto element = vector_data.substr(0, next_comma);
+                vector_data = vector_data.substr(next_comma + 1, vector_data.size() - next_comma);
+                auto obj = TySonObject(element);
+                vector_.emplace_back(obj);
+            }
+        }
+
+    public:
+        explicit TySonObject(std::string_view object)
+        {
+            auto end_type_sep = object.find_first_of('|');
+            auto end_value_sep = (object.size() - 1) - (end_type_sep + 1);
+            auto type = object.substr(0, end_type_sep);
+
+            if (object == "null")
+            {
+                type_ = TySonType::Null;
+                value_ = {};
+            }
+
+            if (object.starts_with("uts"))
+            {
+                type_ = TySonType::Timestamp;
+                value_ = object.substr(end_type_sep + 1, end_value_sep);
+            }
+            else if (object.starts_with('v'))
+            {
+                type_ = TySonType::Vector;
+
+                parse_vector_elements(object);
+            }
+            else if (type.size() > 1)
+            {
+                type_ = TySonType::Link;
+                value_ = object.substr(end_type_sep + 1, end_value_sep);
+            }
+            else
+            {
+                type_ = static_cast<TySonType>(type.at(0));
+                value_ = object.substr(end_type_sep + 1, end_value_sep);
+            }
+        }
+
+        [[nodiscard]] TySonType type() const
+        {
+            return type_;
+        }
+
+        [[nodiscard]] std::string value() const
+        {
+            return value_;
+        }
+
+        template<TySonType T>
+        requires (T == TySonType::Vector)
+        [[nodiscard]] std::vector<TySonObject> value() const
+        {
+            return vector_;
+        }
+
+        template<TySonType T>
+        requires (T == TySonType::Map)
+        [[nodiscard]] std::map<std::string, TySonObject> value() const
+        {
+            return map_;
+        }
+
+        template<typename T>
+        requires std::is_arithmetic_v<T>
+        [[nodiscard]] T value() const
+        {
+            if (type_ == TySonType::Number || type_ == TySonType::Bool || type_ == TySonType::Timestamp)
+            {
+                std::size_t pos{};
+                T result{};
+
+                switch (*typeid(T).name())
+                {
+                    case 's':
+                    case 'j':
+                    case 'i':
+                        result = std::stoi(value_, &pos);
+                        break;
+                    case 'l':
+                        result = std::stol(value_, &pos);
+                        break;
+                    case 'f':
+                        result = static_cast<T>(std::stof(value_, &pos));
+                        break;
+                    case 'd':
+                        result = static_cast<T>(std::stod(value_, &pos));
+                        break;
+                    case 'x':
+                        result = std::stoll(value_, &pos);
+                        break;
+                    case 'e':
+                        result = static_cast<T>(std::stold(value_, &pos));
+                        break;
+                    case 'b':
+                        result = value_ == "true";
+                        break;
+                    case 'c':
+                        result = value_.c_str()[0];
+                        break;
+                }
+                return result;
+            }
+            else
+            {
+                throw std::invalid_argument("Invalid TySonType");
+            }
+        }
+
+    };
+
+
+    class TySonCollectionObject
+    {
+        std::vector<TySonObject> collection_objects_ {};
+
+    public:
+        TySonCollectionObject() = default;
+
+        void add(TySonObject &object)
+        {
+            collection_objects_.emplace_back(object);
+        };
+    };
+
+
+    class TySonData
+    {
+        std::string data_;
+
+    public:
+        explicit TySonData(std::string_view data) : data_(data) {}
+
+        template<const char* key>
+        static std::optional<TySonCollectionObject> get(const TySonData &tysonData)
+        {
+            if (strcmp(key, "objects") == 0 || strcmp(key, "ids") == 0)
+            {
+                auto begin = std::string("s|data|:") + key;
+                if (tysonData.data_.starts_with(begin))
+                {
+                    TySonCollectionObject object {};
+                    // object.add();
+                    return object;
+                }
+            }
+            return {};
+        }
     };
 
 }
---
+++ src/tests/test_tyson_parsing.cpp
@@ -0,0 +1,110 @@
+//
+// Created by felix on 01.02.23.
+//
+#include "gtest/gtest.h"
+#include "../TySON.hpp"
+
+std::string only_map_data = "s|data|:objects{"
+                     "test|d08412fb-9d7e-4d8f-8905-c7355a67ff70|:m{"
+                     "    s|num|:n|0|,"
+                     "    s|name|:s|test_0|,"
+                     "},"
+                     "test|d261580c-1c7f-4cf0-a231-be4a25486146|:m{"
+                     "    s|num|:n|1|,"
+                     "    s|name|:s|test_1|,"
+                     "},"
+                     "test|2b908538-2ec5-4970-8a65-30f7e3f6302c|:m{"
+                     "    s|name|:s|test_2|,"
+                     "    s|num|:n|2|,"
+                     "},"
+                     "test|510eb502-a645-44f4-9ea1-537682fd4049|:m{"
+                     "    s|name|:s|test_3|,"
+                     "    s|num|:n|3|,"
+                     "},"
+                     "test|12b519bd-c7b8-4124-9ba2-8647ef851900|:m{"
+                     "    s|num|:n|4|,"
+                     "    s|name|:s|test_4|,"
+                     "},"
+                     "}";
+
+std::string id_response = "s|data|:ids["
+                          "test|4339ace2-9ab3-4c79-b557-f9b78d66b7f9|,"
+                          "test|3677c916-ac4d-40ab-89f4-def1e565e7ab|,"
+                          "test|5ff00377-34ac-43b9-8ebb-71bb5ff78ebf|,"
+                          "test|7bdd7c8f-e9da-42f6-b473-5a6fd9a1c90f|,"
+                          "]";
+
+TEST(tyson_parsing, create_tyson_object)
+{
+    ASSERT_NO_THROW(tyson::TySonData tysonObject {only_map_data});
+    ASSERT_NO_THROW(tyson::TySonData tysonObject {id_response});
+}
+
+TEST(tyson_parsing, number_bool_type)
+{
+    tyson::TySonObject object {"n|10.12345|"};
+    ASSERT_EQ(object.type(), tyson::TySonType::Number);
+
+    ASSERT_EQ(object.value<int>(), 10);
+    ASSERT_EQ(object.value<short>(), 10);
+    ASSERT_EQ(object.value<unsigned int>(), 10);
+    ASSERT_EQ(object.value<int>(), 10);
+    ASSERT_EQ(object.value<long long>(), 10);
+    ASSERT_FLOAT_EQ(object.value<float>(), float(10.12345));
+    ASSERT_EQ(object.value<double>(), 10.12345);
+    ASSERT_EQ(object.value<bool>(), false);
+}
+
+TEST(tyson_parsing, string_type)
+{
+    tyson::TySonObject long_str_object {"s|Lorem ipsum dolor|"};
+    ASSERT_EQ(long_str_object.type(), tyson::TySonType::String);
+
+    tyson::TySonObject single_digit_str {"s|n|"};
+    ASSERT_EQ(single_digit_str.type(), tyson::TySonType::String);
+
+    ASSERT_THROW([&single_digit_str](){
+        auto val = single_digit_str.value<int>();
+        val *= 2;
+        }(), std::invalid_argument);
+}
+
+TEST(tyson_parsing, null_type)
+{
+    tyson::TySonObject null_object {"null"};
+
+    ASSERT_EQ(null_object.type(), tyson::TySonType::Null);
+    ASSERT_EQ(null_object.value(), "");
+}
+
+TEST(tyson_parsing, uts_type)
+{
+    tyson::TySonObject object {"uts|123456789|"};
+
+    ASSERT_EQ(object.type(), tyson::TySonType::Timestamp);
+    ASSERT_EQ(object.value<long>(), 123456789);
+}
+
+TEST(tyson_parsing, link_type)
+{
+    tyson::TySonObject object {"users|e0bbcda2-0911-495e-9f0f-ce00db489f10|"};
+
+    ASSERT_EQ(object.type(), tyson::TySonType::Link);
+    ASSERT_EQ(object.value(), "e0bbcda2-0911-495e-9f0f-ce00db489f10");
+}
+
+TEST(tyson_parsing, vector_type)
+{
+    tyson::TySonObject object {"v[n|1|,n|2|,n|3|,]"};
+
+    auto expected_value = std::vector<tyson::TySonObject> {};
+//    expected_value.reserve(3);
+
+    expected_value.emplace_back("n|1|");
+    expected_value.emplace_back("n|2|");
+    expected_value.emplace_back("n|3|");
+
+    ASSERT_EQ(object.type(), tyson::TySonType::Vector);
+    ASSERT_EQ(object.value<tyson::TySonType::Vector>().size(), expected_value.size());
+
+}
